cb9b36dae4f60313d8a5cc16c59649be139a5d60
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-19 19:28:02 +0000

    made page responsive on all screen sizes and made page interactive

---
+++ script.js
@@ -0,0 +1,25 @@
+import { data } from "./data.js";
+
+let title = document.querySelector(".jsTitle");
+let text = document.querySelector(".jsText");
+let infoBtn = document.querySelectorAll(".jsInfoBtn");
+
+title.innerHTML = data[0].name;
+text.innerHTML = data[0].text;
+
+infoBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    infoBtn.forEach((btn) => {
+      btn.classList.add("btn");
+      btn.classList.remove("white-bg");
+    });
+    btn.classList.remove("btn");
+    btn.classList.add("white-bg");
+    data.forEach((item) => {
+      if (item.name === btn.dataset.id) {
+        title.innerHTML = item.name;
+        text.innerHTML = item.text;
+      }
+    });
+  });
+});
